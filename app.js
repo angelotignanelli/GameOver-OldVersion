9f57e37b46c59d938ee28c7d99221f1e504afe3a
JonathanAGonzalez <Jonathang@hotmail.com.ar>
2020-03-31 23:49:48 -0300

    Se modifica la ruta en app, esta en /product

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/productos', proyectoRouter);
+app.use('/product', proyectoRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
