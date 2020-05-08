8dce948ec99035c8e31cea576827fddfa47cbcb9
MaLau1979 <valvanoster@gmail.com>
2020-05-08 14:40:30 -0300

    Metiendo Login en el UsersControllers

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,10 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+const {check, validationResult,body}=require('express-validator');
+const bcrypt = require('bcryptjs');
+const usersFilePath = path.join(__dirname,'../data/users.json');
+const usersJSON = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
 controller ={
+    register: function(req, res, next) {
+    res.render('register');
+    },
     login: function(req, res, next) {
         res.render('login');
-      },
-    register: function(req, res, next) {
-        res.render('register');
-      },
+    },
+    processLogin:function(req,res,next){
+      let errors=validationResult(req);
+      if(errors.isEmpty()){
+          let users;
+          if(usersJSON==""){
+              users=[];  
+          }else{
+              users=usersJSON;
+          }
+          let usuarioALoguearse;
+          for(let i=0;i < users.length; i++){
+              if(users[i].email == req.body.email){
+              //console.log(req.body.password + ' ' + users[i].password);
+              let passwordHash=bcrypt.hashSync(users[i].password, 10);
+              if(bcrypt.compareSync(req.body.password, passwordHash)){
+              if(req.body.password==users[i].password){
+              usuarioALoguearse=users[i];
+              break;
+          }
+          }
+          }
+              //console.log(usuarioALoguearse); 
+          if(usuarioALoguearse==undefined){
+              return res.render('login',{errors:[
+              {msg:"Credenciales inválidas"}
+              ]});
+              }
+
+          req.session.usuarioLogueado=usuarioALoguearse;
+          res.send('Success');
+          }
+      }else{
+          return res.render('login',{errors:errors.errors});
+          }
+  },
     fanZone: function(req, res) {
     res.render('fanZone');
     }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResults, body} = require ('express-validator');
+
 const usersController = require('../controller/usersController');
+
 
 router.get('/register', usersController.register);
 router.get('/login', usersController.login);
+router.post('/login', [
+    check('email').isEmail().withMessage("El E-mail es inválido"),
+    check('password').isLength({min:8}).withMessage("El password debe tener como mínimo 8 caracteres")],
+    usersController.processLogin);
+router.get('/check', function(req,res){
+    if(req.session.usuarioLogueado == undefined){
+        res.send("No estás logueado");
+    }else{
+        res.send("El usuario logueado es" + req.session.usuarioLogueado.email)
+    }
+});
 router.get('/fanzone', usersController.fanZone)
 
 
