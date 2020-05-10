f9038ae126bd54cfbced3c6a861fc7a8c1afaf6f
JonathanAGonzalez <jonathang@hotmail.com.ar>
2020-05-10 20:41:36 -0300

    Arreglando home, y registro.

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,7 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 controller ={
     index: function(req, res, next) {
-        res.render('index');
-      }
+      // Do the magic
+      let recomendados = products.filter(function(element) {
+        return element.section == "Recomendados";
+        });
+        let vendidos = products.filter(function(element) {
+        return element.section == "Vendidos";
+        });
+        let ofertas = products.filter(function(element) {
+        return element.section == "Ofertas";
+        });
+        res.render('index', {
+        recomendados: recomendados,
+        vendidos: vendidos,
+        ofertas: ofertas,
+        aMiles: toThousand,
+      });
+      res.render('index',{
+        recomendados:recomendados
+      })
+    }
 }
 
 module.exports = controller;
